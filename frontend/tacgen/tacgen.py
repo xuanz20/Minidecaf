fb99048c8c108cf38c63a5b7114cdcd9dfeb8a10
xuanzhen <xuanz20@mails.tsinghua.edu.cn>
2023-05-29 22:38:03 -0400

    main

---
+++ frontend/tacgen/tacgen.py
@@ -0,0 +1,220 @@
+import utils.riscv as riscv
+from frontend.ast import node
+from frontend.ast.tree import *
+from frontend.ast.visitor import Visitor
+from frontend.symbol.varsymbol import VarSymbol
+from frontend.type.array import ArrayType
+from utils.tac import tacop
+from utils.tac.funcvisitor import FuncVisitor
+from utils.tac.programwriter import ProgramWriter
+from utils.tac.tacprog import TACProg
+from utils.tac.temp import Temp
+
+"""
+The TAC generation phase: translate the abstract syntax tree into three-address code.
+"""
+
+
+class TACGen(Visitor[FuncVisitor, None]):
+    def __init__(self) -> None:
+        pass
+
+    # Entry of this phase
+    def transform(self, program: Program) -> TACProg:
+        mainFunc = program.mainFunc()
+        pw = ProgramWriter(["main"])
+        # The function visitor of 'main' is special.
+        mv = pw.visitMainFunc()
+
+        mainFunc.body.accept(self, mv)
+        # Remember to call mv.visitEnd after the translation a function.
+        mv.visitEnd()
+
+        # Remember to call pw.visitEnd before finishing the translation phase.
+        return pw.visitEnd()
+
+    def visitBlock(self, block: Block, mv: FuncVisitor) -> None:
+        for child in block:
+            child.accept(self, mv)
+
+    def visitReturn(self, stmt: Return, mv: FuncVisitor) -> None:
+        stmt.expr.accept(self, mv)
+        mv.visitReturn(stmt.expr.getattr("val"))
+
+    def visitBreak(self, stmt: Break, mv: FuncVisitor) -> None:
+        mv.visitBranch(mv.getBreakLabel())
+    
+    def visitContinue(self, stmt : Continue, mv : FuncVisitor) -> None :
+        mv.visitBranch(mv.getContinueLabel())
+
+    def visitIdentifier(self, ident: Identifier, mv: FuncVisitor) -> None:
+        """
+        1. Set the 'val' attribute of ident as the temp variable of the 'symbol' attribute of ident.
+        """
+        ident.setattr("val",ident.getattr("symbol").temp)
+
+    def visitDeclaration(self, decl: Declaration, mv: FuncVisitor) -> None:
+        """
+        1. Get the 'symbol' attribute of decl.
+        2. Use mv.freshTemp to get a new temp variable for this symbol.
+        3. If the declaration has an initial value, use mv.visitAssignment to set it.
+        """
+        symbol = decl.getattr("symbol")
+        symbol.temp = mv.freshTemp()
+        if decl.init_expr:
+            decl.init_expr.accept(self,mv)
+            mv.visitAssignment(symbol.temp,decl.init_expr.getattr("val"))
+            #expr.rhs.accept(self,mv)
+            #temp = expr.lhs.getattr("symbol").temp
+            #expr.setattr("val",mv.visitAssignment(temp,expr.rhs.getattr("val")))
+    
+    def visitIf(self, stmt: If, mv: FuncVisitor) -> None:
+        stmt.cond.accept(self, mv)
+
+        if stmt.otherwise is NULL:
+            skipLabel = mv.freshLabel()
+            mv.visitCondBranch(
+                tacop.CondBranchOp.BEQ, stmt.cond.getattr("val"), skipLabel
+            )
+            stmt.then.accept(self, mv)
+            mv.visitLabel(skipLabel)
+        else:
+            skipLabel = mv.freshLabel()
+            exitLabel = mv.freshLabel()
+            mv.visitCondBranch(
+                tacop.CondBranchOp.BEQ, stmt.cond.getattr("val"), skipLabel
+            )
+            stmt.then.accept(self, mv)
+            mv.visitBranch(exitLabel)
+            mv.visitLabel(skipLabel)
+            stmt.otherwise.accept(self, mv)
+            mv.visitLabel(exitLabel)
+
+    def visitWhile(self, stmt: While, mv: FuncVisitor) -> None:
+        beginLabel = mv.freshLabel()
+        loopLabel = mv.freshLabel()
+        breakLabel = mv.freshLabel()
+        mv.openLoop(breakLabel, loopLabel)
+
+        mv.visitLabel(beginLabel)
+        stmt.cond.accept(self, mv)
+        mv.visitCondBranch(tacop.CondBranchOp.BEQ, stmt.cond.getattr("val"), breakLabel)
+
+        stmt.body.accept(self, mv)
+        mv.visitLabel(loopLabel)
+        mv.visitBranch(beginLabel)
+        mv.visitLabel(breakLabel)
+        mv.closeLoop()
+    
+
+    def visitFor(self,stmt: For,mv:FuncVisitor) -> None:
+        if stmt.init is not NULL:
+            stmt.init.accept(self,mv)
+        #stmt.cond.accept(self,mv)
+        beginLabel = mv.freshLabel()
+        loopLabel = mv.freshLabel()
+        breakLabel = mv.freshLabel()
+        mv.openLoop(breakLabel, loopLabel)
+
+        mv.visitLabel(beginLabel)
+        if stmt.cond is not NULL:
+            stmt.cond.accept(self, mv)
+            mv.visitCondBranch(tacop.CondBranchOp.BEQ, stmt.cond.getattr("val"), breakLabel)
+        stmt.body.accept(self, mv)
+        mv.visitLabel(loopLabel)
+        if stmt.update is not NULL:
+            stmt.update.accept(self,mv)
+        mv.visitBranch(beginLabel)
+        mv.visitLabel(breakLabel)
+        mv.closeLoop()
+    
+    def visitDoWhile(self,stmt:DoWhile,mv:FuncVisitor) -> None:
+        beginLabel = mv.freshLabel()
+        loopLabel = mv.freshLabel()
+        breakLabel = mv.freshLabel()
+        mv.openLoop(breakLabel, loopLabel)
+
+        mv.visitLabel(beginLabel)
+        stmt.body.accept(self, mv)
+        stmt.cond.accept(self, mv)
+        mv.visitCondBranch(tacop.CondBranchOp.BEQ, stmt.cond.getattr("val"), breakLabel)
+
+        #stmt.body.accept(self, mv)
+        mv.visitLabel(loopLabel)
+        mv.visitBranch(beginLabel)
+        mv.visitLabel(breakLabel)
+        mv.closeLoop()
+
+
+
+
+
+
+
+    def visitUnary(self, expr: Unary, mv: FuncVisitor) -> None:
+        expr.operand.accept(self, mv)
+
+        op = {
+            node.UnaryOp.Neg: tacop.UnaryOp.NEG,
+            node.UnaryOp.BitNot:tacop.UnaryOp.NOT,
+            node.UnaryOp.LogicNot:tacop.UnaryOp.SEQZ,
+            # You can add unary operations here.
+        }[expr.op]
+        expr.setattr("val", mv.visitUnary(op, expr.operand.getattr("val")))
+
+    def visitBinary(self, expr: Binary, mv: FuncVisitor) -> None:
+        expr.lhs.accept(self, mv)
+        expr.rhs.accept(self, mv)
+
+        op = {
+            node.BinaryOp.Add: tacop.BinaryOp.ADD,
+            node.BinaryOp.Sub: tacop.BinaryOp.SUB,
+            node.BinaryOp.Mul: tacop.BinaryOp.MUL,
+            node.BinaryOp.Div: tacop.BinaryOp.DIV,
+            node.BinaryOp.Mod: tacop.BinaryOp.REM,
+            node.BinaryOp.LE:  tacop.BinaryOp.LEQ,
+            node.BinaryOp.LT:  tacop.BinaryOp.SLT,
+            node.BinaryOp.GE:  tacop.BinaryOp.GEQ,
+            node.BinaryOp.GT:  tacop.BinaryOp.SGT,
+            node.BinaryOp.EQ:  tacop.BinaryOp.EQU,
+            node.BinaryOp.NE:  tacop.BinaryOp.NEQ,
+            node.BinaryOp.LogicAnd: tacop.BinaryOp.AND,
+            node.BinaryOp.LogicOr: tacop.BinaryOp.OR,
+            # You can add binary operations here.
+        }[expr.op]
+        expr.setattr(
+            "val", mv.visitBinary(op, expr.lhs.getattr("val"), expr.rhs.getattr("val"))
+        )
+
+    def visitAssignment(self, expr: Assignment, mv: FuncVisitor) -> None:
+        """
+        1. Visit the right hand side of expr, and get the temp variable of left hand side.
+        2. Use mv.visitAssignment to emit an assignment instruction.
+        3. Set the 'val' attribute of expr as the value of assignment instruction.
+        """
+        #expr.lhs.accept(self,mv)
+        expr.rhs.accept(self,mv)
+        temp = expr.lhs.getattr("symbol").temp
+        expr.setattr("val",mv.visitAssignment(temp,expr.rhs.getattr("val")))
+
+
+    def visitCondExpr(self, expr: ConditionExpression, mv: FuncVisitor) -> None:
+        """
+        1. Refer to the implementation of visitIf and visitBinary.
+        """
+        expr.cond.accept(self, mv)
+
+        skipLabel = mv.freshLabel()
+        exitLabel = mv.freshLabel()
+        mv.visitCondBranch(tacop.CondBranchOp.BEQ, expr.cond.getattr("val"), skipLabel)
+        expr.then.accept(self, mv)
+        mv.visitBranch(exitLabel)
+        mv.visitLabel(skipLabel)
+        expr.otherwise.accept(self, mv)
+        mv.visitAssignment(expr.then.getattr("val"),expr.otherwise.getattr("val"))
+        mv.visitLabel(exitLabel)
+        expr.setattr("val",expr.then.getattr("val"))
+
+
+    def visitIntLiteral(self, expr: IntLiteral, mv: FuncVisitor) -> None:
+        expr.setattr("val", mv.visitLoad(expr.value))
